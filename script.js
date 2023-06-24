2e6b30b033b15a8135b2ba81ba3a2e096563743d
ManfredNelvin <119521670+ManfredNelvin@users.noreply.github.com>
2023-06-24 15:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,26 @@
-// Your Script here.
+function rot13(encodedString) {
+  let decodedString = "";
 
-const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
+  for (let i = 0; i < encodedString.length; i++) {
+    const charCode = encodedString.charCodeAt(i);
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+    if (charCode >= 65 && charCode <= 90) {
+      let decodedCharCode = charCode - 13;
 
-  return; //return decodedArr
+      if (decodedCharCode < 65) {
+        decodedCharCode += 26;
+      }
+
+      decodedString += String.fromCharCode(decodedCharCode);
+    } else {
+      decodedString += encodedString.charAt(i);
+    }
+  }
+
+  return decodedString;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
+// Example usage
+const encodedString = "EBG13 rknzcyr.";
+const decodedString = rot13(encodedString);
+console.log(decodedString); // "ROT13 example."
